c14aaaca030f7c16084d6cd2b85d3a9fb9f3a997
Daniel Caldera <bdcaldera@gmail.com>
2020-05-18 01:02:36 -0500

    delete

--- Main/app/auth/views.py
+++ Main/app/auth/views.py
@@ -1,8 +1,9 @@
 from flask import render_template, session, redirect, url_for, flash # pylint: disable=import-error
 from flask_login import login_user, login_required, logout_user # pylint: disable=import-error
+from werkzeug.security import generate_password_hash, check_password_hash # pylint: disable=import-error
 
 from app.forms import LoginForm
-from app.firestore_service import get_user
+from app.firestore_service import get_user, user_put # pylint: disable=no-name-in-module
 from app.models import UserModel, UserData
 
 from . import auth
@@ -20,10 +21,9 @@ def login():
         user_doc = get_user(username)
 
         if user_doc.to_dict() is not None:
-            password_from_db = user_doc.to_dict()['password']
 
-            if password == password_from_db:
-                user_data = UserData(username, password)
+            if check_password_hash(user_doc.to_dict()['password'], password):
+                user_data = UserData(username,password)
                 user = UserModel(user_data)
 
                 login_user(user)
@@ -39,6 +39,37 @@ def login():
         return redirect(url_for('index'))
 
     return render_template('login.html', **context)
+
+@auth.route('signup', methods=['GET', 'POST'])
+def signup():
+    signup_form = LoginForm()
+    context = {
+        'signup_form': signup_form
+    }
+
+    if signup_form.validate_on_submit():
+        username = signup_form.username.data
+        password = signup_form.password.data
+
+        user_doc = get_user(username)
+
+        if user_doc.to_dict() is None:
+            password_hash = generate_password_hash(password)
+            user_data = UserData(username, password_hash)
+            user_put(user_data)
+
+            user = UserModel(user_data)
+
+            login_user(user)
+
+            flash('Bienvenido!')
+
+            return redirect(url_for('hello'))
+
+        else:
+            flash('El usario existe!')
+
+    return render_template('signup.html', **context)
 
 @auth.route('/logout')
 @login_required
--- Main/app/forms.py
+++ Main/app/forms.py
@@ -2,8 +2,14 @@ from flask_wtf import FlaskForm # pylint: disable=import-error
 from wtforms.fields import StringField, PasswordField, SubmitField # pylint: disable=import-error
 from wtforms.validators import DataRequired # pylint: disable=import-error
 
-
 class LoginForm(FlaskForm):
     username = StringField('Nombre de usuario', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Enviar')
+
+class TodoForm(FlaskForm):
+    description = StringField('Descripción', validators=[DataRequired()])
+    submit = SubmitField('Crear')
+
+class DeleteTodoForm(FlaskForm):
+    submit = SubmitField('Borrar')
